f63f3b72340300be01a1f6b2e5d78fdf7725a65a
channmixx <774716196@qq.com>
2019-12-02 16:36:35 +0800

    submit week13(12.2) class code

---
+++ linuxprogram/week13/code/my.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <wait.h>
+#include <errno.h>
+#include <signal.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <time.h>
